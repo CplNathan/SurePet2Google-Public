06b18ec2d67b4e369e92f83a02b3b4f9d45ba22d
Nathan <cplnathan2001@gmail.com>
2023-07-21 10:58:56 +0100

    Update HttpContext.cs

--- SurePet2Google.Blazor/Server/Context/HttpContext.cs
+++ SurePet2Google.Blazor/Server/Context/HttpContext.cs
@@ -1,8 +1,7 @@
-﻿using Flurl.Http;
+using Flurl.Http;
 using Flurl.Http.Configuration;
 using Microsoft.Extensions.Http;
 using Polly;
-using Polly.Contrib.DuplicateRequestCollapser;
 using System.Net;
 
 namespace SurePet2Google.Blazor.Server.Context
@@ -17,19 +16,17 @@ namespace SurePet2Google.Blazor.Server.Context
             return policy;
         }
 
-        private static IAsyncPolicy<HttpResponseMessage> BuildRetryPolicy()
+        public static IAsyncPolicy<HttpResponseMessage> BuildRetryPolicy()
         {
             var retryPolicy =
-                AsyncRequestCollapserPolicy.Create().WrapAsync(
-                    Policy
-                   .Handle((FlurlHttpException exception) => new List<int>() { (int)HttpStatusCode.TooManyRequests, (int)HttpStatusCode.BadRequest }.Contains(exception.StatusCode ?? (int)HttpStatusCode.BadRequest))
-                   .WaitAndRetryAsync(5, retryAttempt =>
-                   {
-                       var nextAttemptIn = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
-                       Console.WriteLine($"Retry attempt {retryAttempt} to make request. Next try on {nextAttemptIn.TotalSeconds} seconds.");
-                       return nextAttemptIn;
-                   })
-               ).AsAsyncPolicy<HttpResponseMessage>();
+                Policy
+                .Handle<HttpRequestException>((exception) => new List<HttpStatusCode>() { HttpStatusCode.TooManyRequests, HttpStatusCode.BadRequest }.Contains(exception.StatusCode ?? HttpStatusCode.BadRequest))
+                .WaitAndRetryAsync(5, retryAttempt =>
+                {
+                    var nextAttemptIn = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+                    Console.WriteLine($"Retry attempt {retryAttempt} to make request. Next try on {nextAttemptIn.TotalSeconds} seconds.");
+                    return nextAttemptIn;
+                }).AsAsyncPolicy<HttpResponseMessage>();
 
             return retryPolicy;
         }
